6069b90e5382081f2849a5faa748dff59578872a
Debashish Saha <ashish.saha01@gmail.com>
2016-10-19 14:23:15 -0400

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -65,7 +65,7 @@ public class SJF  {
 			IOqueue.add(temp);
 		}
 		
-		System.out.println("\n" +"schduling algorithm " + algorithm + " current CPU clock " + timer + " average processing time " + (totalProcessingTime/numJobs) + " average waiting time " + (totalWaitingTime/numJobs) + " average tunaround time " + (totalTurnaroundTime/numJobs));
+		System.out.println("\n" +"scheduling algorithm " + algorithm + " current CPU clock " + timer + " average processing time " + (totalProcessingTime/numJobs) + " average waiting time " + (totalWaitingTime/numJobs) + " average turnaround time " + (totalTurnaroundTime/numJobs));
 
 	}
 }
